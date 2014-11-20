62abb5a81fd1463d8a11bb8ba4ae835ed89455c8
fvisconti <france.visconti@gmail.com>
2014-11-20 12:30:31 +0100

    Improved comments on functions behaviour

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,11 @@
 ## taking care about the "newness" of it: if the matrix is already
 ## known, its inverse is loaded from cache where it was previously
 ## stored.
+## What is going on is seen if you use the "set" function to
+## initialize a new matrix: when you call "set(y)" you assign
+## the y matrix to x in the global environment and put m to NULL
+## in the global environment as well, so that the cacheSolve 
+## function will read those new values.
 
 ## mCM function takes a matrix and defines a bunch of functions
 ## to work with it, returning a list
@@ -20,7 +25,7 @@ makeCacheMatrix <- function(x = matrix()) {
        getsolve = getsolve)
 }
 
-## cS function has to be used after mCM is instantiated, like this:
+## cS function has to be used together with mCM, like this:
 ## cacheSolve(makeCacheMatrix(x)), and will return the inverse of 
 ## matrix x, reading it from cache if already computed
 
